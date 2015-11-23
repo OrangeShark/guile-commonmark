33de23db98ed8db4bea2094aa3995247b78dc2ce
Erik Edrosa <erik.edrosa@gmail.com>
2015-11-23 18:26:47 -0500

    Add inlines.scm

---
+++ commonmark/inlines.scm
@@ -0,0 +1,12 @@
+(define-module (commonmark inlines)
+  #:use-module (commonmark node)
+  #:export (parse-inlines))
+
+
+;; Document -> Document
+;; parses the lines of the document
+;; !!!
+
+(define (parse-inlines d)
+  d)
+
