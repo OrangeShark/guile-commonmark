3d8c62eef95e931cb00ecd1bbe37f0199f4281d6
Erik Edrosa <erik.edrosa@gmail.com>
2015-11-29 23:01:24 -0500

    add license

--- commonmark/blocks.scm
+++ commonmark/blocks.scm
@@ -1,3 +1,20 @@
+;; Copyright (C) 2015  Erik Edrosa <erik.edrosa@gmail.com>
+;;
+;; This file is part of guile-commonmark
+;;
+;; guile-commonmark is free software: you can redistribute it and/or
+;; modify it under the terms of the GNU Lesser General Public License
+;; as published by the Free Software Foundation, either version 3 of
+;; the License, or (at your option) any later version.
+;;
+;; guile-commonmark is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU Lesser General Public License for more details.
+;;
+;; You should have received a copy of the GNU Lesser General Public License
+;; along with guile-commonmark.  If not, see <http://www.gnu.org/licenses/>.
+
 (define-module (commonmark blocks) 
   #:use-module (ice-9 regex)
   #:use-module (ice-9 rdelim)
