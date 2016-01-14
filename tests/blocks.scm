154c3fdbec4c905aebbeedcf96fbf07d6a4a2526
Erik Edrosa <erik.edrosa@gmail.com>
2016-01-14 18:30:06 -0500

    add fenced code tests

--- tests/blocks.scm
+++ tests/blocks.scm
@@ -338,6 +338,152 @@
                 #t)
                (x (pk 'fail x #f))))
 
+(test-assert "parse-blocks, fenced code simple"
+             (match (call-with-input-string "```\n<\n >\n```" parse-blocks)
+               (('document doc-data
+                           ('fenced-code code-data
+                                         "<\n >"))
+                #t)
+               (x (pk 'fail x #f))))
+
+(test-assert "parse-blocks, fenced code simple with tildes"
+             (match (call-with-input-string "~~~\n<\n >\n~~~" parse-blocks)
+               (('document doc-data
+                           ('fenced-code code-data
+                                         "<\n >"))
+                #t)
+               (x (pk 'fail x #f))))
+
+(test-assert "parse-blocks, fenced code must use the same character as opening fence"
+             (match (call-with-input-string "```\naaa\n~~~\n```" parse-blocks)
+               (('document doc-data
+                           ('fenced-code code-data
+                                         "aaa\n~~~"))
+                #t)
+               (x (pk 'fail x #f))))
+
+(test-expect-fail 1)
+(test-assert "parse-blocks, fenced code must use the same character as opening fence tildes"
+             (match (call-with-input-string "````\naaa\n```\n````" parse-blocks)
+               (('document doc-data
+                           ('fenced-code code-data
+                                         "aaa\n```"))
+                #t)
+               (x (pk 'fail x #f))))
+
+(test-assert "parse-blocks, fenced code unclosed until end of the document"
+             (match (call-with-input-string "```" parse-blocks)
+               (('document doc-data
+                           ('fenced-code code-data))
+                #t)
+               (x (pk 'fail x #f))))
+
+(test-assert "parse-blocks, fenced code closes at end of block quote"
+             (match (call-with-input-string "> ```\n> aaa\n\nbbb" parse-blocks)
+               (('document doc-data
+                           ('paragraph para-data
+                                       ('text text-data "bbb"))
+                           ('block-quote quote-data
+                                         ('fenced-code code-data
+                                                       "aaa")))
+                #t)
+               (x (pk 'fail x #f))))
+
+(test-assert "parse-blocks, fenced code can have all empty lines"
+             (match (call-with-input-string "```\n\n  \n```" parse-blocks)
+               (('document doc-data
+                           ('fenced-code code-data
+                                         "\n  "))
+                #t)
+               (x (pk 'fail x #f))))
+
+(test-assert "parse-blocks, fenced code can be empty"
+             (match (call-with-input-string "```\n```" parse-blocks)
+               (('document doc-data
+                           ('fenced-code code-data))
+                #t)
+               (x (pk 'fail x #f))))
+
+(test-expect-fail 3)
+(test-assert "parse-blocks, fenced code can be indented and equivalent spaces removed"
+             (match (call-with-input-string " ```\n aaa\naaa\n```" parse-blocks)
+               (('document doc-data
+                           ('fenced-code code-data
+                                         "aaa\naaa"))
+                #t)
+               (x (pk 'fail x #f))))
+
+(test-assert "parse-blocks, fenced code can be indented and equivalent spaces removed if present"
+             (match (call-with-input-string "  ```\naaa\n  aaa\naaa\n  ```" parse-blocks)
+               (('document doc-data
+                           ('fenced-code code-data
+                                         "aaa\naaa\naaa"))
+                #t)
+               (x (pk 'fail x #f))))
+
+(test-assert "parse-blocks, fenced code closing fence 4 spaces too much"
+             (match (call-with-input-string "```\naaa\n    ```" parse-blocks)
+               (('document doc-data
+                           ('fenced-code code-data
+                                         "aaa\n    ```"))
+                #t)
+               (x (pk 'fail x #f))))
+
+(test-assert "parse-blocks, fenced code spaces are not allowed in opening fence"
+             (match (call-with-input-string "``` ```\naaa" parse-blocks)
+               (('document doc-data
+                           ('paragraph para-data
+                                       ('text text-data "aaa" "``` ```")))
+                #t)
+               (x (pk 'fail x #f))))
+
+(test-expect-fail 1)
+(test-assert "parse-blocks, fenced code spaces are not allowed in closing fence"
+             (match (call-with-input-string "~~~~~\naaa\n~~~ ~~" parse-blocks)
+               (('document doc-data
+                           ('fenced-code code-data
+                                         "aaa\n~~~ ~~"))
+                #t)
+               (x (pk 'fail x #f))))
+
+(test-assert "parse-blocks, fenced code can interrupt paragraphs and followed by paragraphs"
+             (match (call-with-input-string "foo\n```\nbar\n```\nbaz" parse-blocks)
+               (('document doc-data
+                           ('paragraph para-data1
+                                       ('text text-data1 "baz"))
+                           ('fenced-code code-data
+                                         "bar")
+                           ('paragraph para-data2
+                                       ('text text-data2 "foo")))
+                #t)
+               (x (pk 'fail x #f))))
+
+(define (info-string data)
+  (assq-ref data 'info-string))
+
+(test-assert "parse-blocks, fenced code info string"
+             (match (call-with-input-string "```scheme\n(define (foo x)\n  3)\n```" parse-blocks)
+               (('document doc-data
+                           ('fenced-code code-data
+                                         "(define (foo x)\n  3)"))
+                (equal? (info-string code-data) "scheme"))
+               (x (pk 'fail x #f))))
+
+(test-assert "parse-blocks, fenced code info string empty code"
+             (match (call-with-input-string "```;\n```" parse-blocks)
+               (('document doc-data
+                           ('fenced-code code-data))
+                (equal? (info-string code-data) ";"))
+               (x (pk 'fail x #f))))
+
+(test-expect-fail 1)
+(test-assert "parse-blocks, fenced code coding fence cannot have info strings"
+             (match (call-with-input-string "```\n``` aaa\n```" parse-blocks)
+               (('document doc-data
+                           ('fenced-code code-data "``` aaa"))
+                (not (equal? (info-string code-data) "aaa")))
+               (x (pk 'fail x #f))))
+
 (test-end)
 
 (exit (= (test-runner-fail-count (test-runner-current)) 0))
