eab3ee901ca947b0d59a4b6db17c96991104c1a8
Erik Edrosa <erik.edrosa@gmail.com>
2016-04-22 22:29:42 -0400

    add a text type for inlines

--- commonmark/inlines.scm
+++ commonmark/inlines.scm
@@ -24,17 +24,40 @@
 (define re-ticks (make-regexp "`+"))
 (define re-main (make-regexp "^[^`]+"))
 
-(define (start-ticks? text position)
-  (regexp-exec re-start-ticks text position))
+(define (start-ticks? text)
+  (regexp-exec re-start-ticks (text-value text) (text-position text)))
 
-(define (end-ticks? text position)
-  (regexp-exec re-ticks text position))
+(define (end-ticks? text)
+  (regexp-exec re-ticks (text-value text) (text-position text)))
 
-(define (normal-text? text position)
-  (regexp-exec re-main text position))
+(define (normal-text? text)
+  (regexp-exec re-main (text-value text) (text-position text)))
 
 (define (match-length match)
   (string-length (match:substring match 0)))
+
+(define (make-text text position)
+  (cons text position))
+
+(define (text-value text)
+  (car text))
+
+(define (text-position text)
+  (cdr text))
+
+(define (text-advance text position)
+  (make-text (text-value text) position))
+
+(define (text-substring text start end)
+  (substring (text-value text) start end))
+
+(define (text-char text)
+  (string-ref (text-value text) (text-position text)))
+
+(define (text-end? text)
+  (>= (text-position text) (string-length (text-value text))))
+
+
 ;; Node -> Node
 ;; parses the inline text of paragraphs and heading nodes
 (define (parse-inlines node)
@@ -42,31 +65,31 @@
         ((or (paragraph-node? node) (heading-node? node)) (parse-inline node))
         (else (make-node (node-type node) (node-data node) (map parse-inlines (node-children node))))))
 
-(define (parse-ticks text position nodes)
-  (let ((start-ticks (start-ticks? text position)))
-    (let loop ((end-ticks (end-ticks? text (match:end start-ticks 0))))
+(define (parse-ticks text nodes)
+  (let ((start-ticks (start-ticks? text)))
+    (let loop ((end-ticks (end-ticks? (text-advance text (match:end start-ticks 0)))))
       (cond ((not end-ticks)
-             (parse-char text (match:end start-ticks 0)
+             (parse-char (text-advance text (match:end start-ticks 0))
                          (cons (make-text-node (match:substring start-ticks 0)) nodes)))
             ((= (match-length start-ticks) (match-length end-ticks))
-             (parse-char text (match:end end-ticks 0)
-                         (cons (make-code-span-node (substring text (match:end start-ticks 0)
-                                                               (match:start end-ticks 0)))
+             (parse-char (text-advance text (match:end end-ticks 0))
+                         (cons (make-code-span-node (text-substring text (match:end start-ticks 0)
+                                                                    (match:start end-ticks 0)))
                                nodes)))
-            (else (loop (end-ticks? text (match:end end-ticks 0))))))))
+            (else (loop (end-ticks? (text-advance text (match:end end-ticks 0)))))))))
 
-(define (parse-normal-text text position nodes)
-  (let ((normal-text (normal-text? text position)))
-    (parse-char text (match:end normal-text 0)
+(define (parse-normal-text text nodes)
+  (let ((normal-text (normal-text? text)))
+    (parse-char (text-advance text (match:end normal-text 0))
                 (cons (make-text-node (match:substring normal-text 0)) nodes))))
 
-(define (parse-char text position nodes)
-  (if (>= position (string-length text))
+(define (parse-char text nodes)
+  (if (text-end? text)
       nodes
-      (case (string-ref text position)
-        ((#\`) (parse-ticks text position nodes))
-        (else (parse-normal-text text position nodes)))))
+      (case (text-char text)
+        ((#\`) (parse-ticks text nodes))
+        (else (parse-normal-text text nodes)))))
 
 (define (parse-inline node)
   (let ((text (last-child (last-child node))))
-    (make-node (node-type node) (node-data node) (parse-char text 0 '()))))
+    (make-node (node-type node) (node-data node) (parse-char (make-text text 0) '()))))
